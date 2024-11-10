06c156667523a7c1c10b8c29785052cee1e62c70
suvojit213 <suvojitsengupta21@gmail.com>
2024-11-10 14:52:02 +0000

    sky: Add flag to enable custom apps
    Added the ADD_CUSTOM_APPS := true flag in pixelstar_sky.mk to enable custom apps integration.

--- pixelstar_sky.mk
+++ pixelstar_sky.mk
@@ -19,6 +19,7 @@ TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_BOOT_ANIMATION_RES := 1080
 USE_PIXEL_CHARGER := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
+ADD_CUSTOM_APPS := true
 
 # Inherit device configurations
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
